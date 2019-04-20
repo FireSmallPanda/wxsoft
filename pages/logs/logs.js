9c62fa2ccf778fcd05942889296e679a6f2a004a
lingweihao <1020529941@qq.com>
2019-04-20 23:34:36 +0800

    add:添加后端请求

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -3,13 +3,43 @@ const util = require('../../utils/util.js')
 
 Page({
   data: {
-    logs: []
+    contentList: []
   },
   onLoad: function () {
-    this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(log => {
-        return util.formatTime(new Date(log))
-      })
+    this.getDataList()
+  },
+  callPhone(){
+    wx.makePhoneCall({
+      phoneNumber: '13107721069' // 仅为示例，并非真实的电话号码
     })
+  },
+  getDataList(){
+    wx.request({
+      url: 'https://www.woheyun.com/api/cgwas/systemAction/getSystemDicts.json', //仅为示例，并非真实的接口地址
+      success:  (res)=> {
+        this.setData({
+          contentList: res.data.data
+        })
+      }
+    })
+  },
+  copyData(data){
+    if (wx.setClipboardData){
+      wx.setClipboardData({
+        data: data.currentTarget.dataset.text,
+        success(res) {
+          wx.showModal({
+            title: '成功',
+            content: '您选取的内容复制成功！',
+          })
+        }
+      })
+    }else{
+      wx.showModal({
+        title: '提示',
+        content: '您的微信版本过低请升级！'
+      })
+    }
+   
   }
 })
