21084e1bb6dcecf59242310f24ea1713a2312b41
weihao_ling <1020529941@qq.com>
2020-09-30 10:13:38 +0800

    fix:更改计费规则

--- utils/util.js
+++ utils/util.js
@@ -15,39 +15,54 @@ const formatNumber = n => {
 }
 const getDifValue = (nowtimestamp, beforetimestamp) => {
   var difValue = nowtimestamp - beforetimestamp;
-  var day = Math.floor(difValue / 1000 / 60 / 60 / 24);//天
+  var day = Math.floor(difValue / 1000 / 60 / 60 / 24); //天
   difValue = difValue % (1000 * 60 * 60 * 24);
-  var hour = Math.floor(difValue / 1000 / 60 / 60);//小时
+  var hour = Math.floor(difValue / 1000 / 60 / 60); //小时
   difValue = difValue % (1000 * 60 * 60);
-  var min = Math.floor(difValue / 1000 / 60);//分钟
+  var min = Math.floor(difValue / 1000 / 60); //分钟
   difValue = difValue % (1000 * 60);
   var second = Math.floor(difValue / 1000);
   let returnString = second + "秒"
-  if(min){
+  if (min) {
     returnString = min + "分钟" + returnString
   }
-  if(hour){
+  if (hour) {
     returnString = hour + "小时" + returnString
   }
-  if(day){
+  if (day) {
     returnString = day + "天" + returnString
   }
-  return  returnString
+  return returnString
 }
 /**
  * 
  * @param {*} nowtimestamp 
  */
 const matchPrice = (time) => {
-  var day = Math.floor(difValue / 1000 / 60 / 60 / 24);//天
+  // 分为两种 一种是138 90分钟 超出后每分钟按照1.5元收费
+  // 另外一种88元 90分钟 超出后按照每分钟1元
+  let rules = [{
+      name: "A",
+      limitPrice: 138,
+      limitTime: 90,
+      evenTimePrice: 1.5
+    },
+    {
+      name: "B",
+      limitPrice: 88,
+      limitTime: 90,
+      evenTimePrice: 1
+    }
+  ]
+  var day = Math.floor(difValue / 1000 / 60 / 60 / 24); //天
   difValue = difValue % (1000 * 60 * 60 * 24);
-  var hour = Math.floor(difValue / 1000 / 60 / 60);//小时
+  var hour = Math.floor(difValue / 1000 / 60 / 60); //小时
   difValue = difValue % (1000 * 60 * 60);
-  var min = Math.floor(difValue / 1000 / 60);//分钟
+  var min = Math.floor(difValue / 1000 / 60); //分钟
   difValue = difValue % (1000 * 60);
   var second = Math.floor(difValue / 1000);
-  let returnString = second + "秒" 
-  
+  let returnString = second + "秒"
+
 
 
 }
